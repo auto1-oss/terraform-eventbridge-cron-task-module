8dd69ba902b4dee8e30e35b38082a61ec23b1d47
sun <alex.sun@auto1.com>
2025-04-17 13:45:55 +0200

    Add license headers

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+#
+# This file is part of the auto1-oss/terraform-eventbridge-cron-task-module.
+#
+# (c) AUTO1 Group SE https://www.auto1-group.com
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+
 locals {
 
   # TODO: Define log configuration based on user input and launch type
